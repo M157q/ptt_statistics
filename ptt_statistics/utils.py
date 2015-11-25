f62fa815e480d49d3c722bd13a8ebc1cdae15117
shunyi <M157q.tw@gmail.com>
2015-11-25 23:45:28 +0800

    Add utils.check_date_tuple()

--- ptt_statistics/utils.py
+++ ptt_statistics/utils.py
@@ -1,3 +1,5 @@
+import calendar
+import datetime
 import os
 
 from . import constants
@@ -6,3 +8,31 @@ from . import constants
 def create_dir_if_not_exists():
     if not os.path.isdir(constants.dir_path):
         os.mkdir(constants.dir_path)
+
+
+def check_date_tuple(date_tuple):
+    year, month, day = date_tuple
+
+    if year is None:
+        errmsg = "year cannot be emtpy."
+        raise ValueError(errmsg)
+
+    current_year = datetime.datetime.today().year
+    if year is not None and not 1 <= year <= current_year:
+        errmsg = "Invalid year range. Should be 1 ~ {}.".format(current_year)
+        raise ValueError(errmsg)
+
+    if month is not None and not 1 <= month <= 12:
+        errmsg = "Invalid month range. Should be 1 ~ 12."
+        raise ValueError(errmsg)
+
+    try:
+        valid_day = calendar.monthrange(year, month)[1]
+    except:
+        pass
+    else:
+        if day is not None and not 1 <= day <= valid_day:
+            errmsg = "Invalid day range. Should be 1 ~ {}.".format(valid_day)
+            raise ValueError(errmsg)
+
+    return (year, month, day)
